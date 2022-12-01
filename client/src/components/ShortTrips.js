802f21128088c507132099693002cd73ee6734c2
Mayank Shiroya <Mshiroya1369@conestogac.on.ca>
2022-12-01 09:40:31 -0500

    added async json mapping

--- client/src/components/ShortTrips.js
+++ client/src/components/ShortTrips.js
@@ -1,42 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Trip from "./Trip";
 import { trip_metadata_json } from "../links";
 import { ShortTripsObject } from "./Generic/TripConstant";
 
-const ShortTrips = () => {
+const ShortTrips = async () => {
     let shortTripDetails;
+    const apiurl = 'http://localhost:5000/service'
+    const [showTrips, setShowTrips] = useState()
+    let displayData
 
     //To Fetch MetaData JSON
-    useEffect(() => {
-        fetch(trip_metadata_json)
-            .then((Response) => Response.json())
-            .then((data) => {
-                shortTripDetails = data.trip_metadata.short_trips_metadata;
-            });
-    });
+    // useEffect(() => {
+    //     fetch(trip_metadata_json)
+    //         .then((Response) => Response.json())
+    //         .then((data) => {
+    //             shortTripDetails = data.trip_metadata.short_trips_metadata;
+    //         });
+    // });
 
-    return (
-        <ul>
-            {
-                //Mapping over MetaDataObject
-                ShortTripsObject.map((value, key) => (
-                    <Trip
-                        tripimageURL={value.tripImageURL}
-                        agencyLogo={value.travelPartnerLogoURL}
-                        tripName={value.destinationName}
-                        agencyName={value.travelPartnerName}
-                        stars={value.reviewStar}
-                        description={value.tripDescription}
-                        travelTime={value.travelTime}
-                        stayTime={value.stayTime}
-                        price={value.tripPrice}
-                        key={key}
-                        id={value.tripId}
-                    />
-                ))
-            }
-        </ul>
-    );
+    const response = await fetch(apiurl)
+    const responseData = await response.json()
+    displayData = responseData.map(function (value) {
+
+        return (
+            <ul>
+
+
+
+                <Trip
+                    tripimageURL={value.tripImageURL}
+                    agencyLogo={value.travelPartnerLogoURL}
+                    tripName={value.destinationName}
+                    agencyName={value.travelPartnerName}
+                    stars={value.reviewStar}
+                    description={value.tripDescription}
+                    travelTime={value.travelTime}
+                    stayTime={value.stayTime}
+                    price={value.tripPrice}
+                    key={key}
+                    id={value.tripId}
+                />
+
+
+            </ul>
+        );
+    })
 };
 
 export default ShortTrips;
